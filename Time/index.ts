698b5ffbefc2b1d2b491b3668174c1c3b0f6abff
Tobias Liljeblad <tobias@issuefab.com>
2023-10-10 15:19:39 +0200

    add modified

--- Time/index.ts
+++ Time/index.ts
@@ -1,12 +1,12 @@
 import { isoly } from "isoly"
+import { userwidgets } from "@userwidgets/model"
 import { Changeable as TimeChangeable } from "./Changeable"
 import { Creatable as TimeCreatable } from "./Creatable"
 
 export interface Time extends Time.Creatable {
 	balance: isoly.TimeSpan
 	locked?: true
-	modified: isoly.DateTime
-	created: isoly.DateTime
+	modified: { time: isoly.DateTime; by: userwidgets.Email }
 }
 export namespace Time {
 	export type Creatable = TimeCreatable
