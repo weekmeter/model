409d67e6161e3056ca21f2a89cae7f6fbe59c612
Tobias Liljeblad <tobias@issuefab.com>
2023-10-11 11:46:30 +0200

    ACtivity

--- Activity/index.ts
+++ Activity/index.ts
@@ -1,4 +1,5 @@
 import { isoly } from "isoly"
+import { isly } from "isly"
 import { Creatable as ActivityCreatable } from "./Creatable"
 
 export interface Activity extends Activity.Creatable {
@@ -8,4 +9,10 @@ export interface Activity extends Activity.Creatable {
 export namespace Activity {
 	export type Creatable = ActivityCreatable
 	export const Creatable = ActivityCreatable
+	export const type: isly.object.ExtendableType<Activity> = Creatable.type.extend<Activity>({
+		modified: isly.fromIs<isoly.DateTime>("DateTime", isoly.DateTime.is),
+		created: isly.fromIs<isoly.DateTime>("DateTime", isoly.DateTime.is),
+	})
+	export const is = type.is
+	export const flaw = type.flaw
 }
