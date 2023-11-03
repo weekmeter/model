f41dec620a941b9300a4f8284148447f41a67be2
Linus Johansson <linus@issuefab.com>
2023-11-03 10:45:39 +0100

    create code from name

--- Code/index.ts
+++ Code/index.ts
@@ -5,4 +5,11 @@ export namespace Code {
 	export const type = isly.string(/^[a-z0-9_-]+$/)
 	export const is = type.is
 	export const flaw = type.flaw
+
+	export function create(name: string): string {
+		return name
+			.toLocaleLowerCase()
+			.replace(/\s+/g, "-")
+			.replace(/[^\w-]/g, "")
+	}
 }
