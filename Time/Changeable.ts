e2c28b17ee4d28abdeaeffe17874f79a8cc093a2
Elias Eriksson <elias@issuefab.com>
2023-11-06 13:33:10 +0100

    fix: Changable.is resolves to Changable (#11)

--- Time/Changeable.ts
+++ Time/Changeable.ts
@@ -1,8 +1,8 @@
 import { Creatable } from "./Creatable"
 
-export type Changeable = Creatable
+export type Changeable = Pick<Creatable, keyof Creatable>
 export namespace Changeable {
-	export const type = Creatable.type
+	export const type = Creatable.type.extend<Changeable>({})
 	export const is = type.is
 	export const flaw = type.flaw
 }
