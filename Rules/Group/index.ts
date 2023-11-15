a2d07b5daefd7417d2b983087c06b606525a118b
Elias Eriksson <eliaseriksson95@gmail.com>
2023-11-15 14:27:15 +0100

    not specifying index in import

--- Rules/Group/index.ts
+++ Rules/Group/index.ts
@@ -2,7 +2,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { isly } from "isly"
 import { Modified } from "../../Modified"
-import { Rule } from "../../Rule/index"
+import { Rule } from "../../Rule"
 import { Changeable as GroupChangeable } from "./Changeable"
 
 export interface Group {
