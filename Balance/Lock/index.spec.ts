a073a1e8417879ba076cb07d261979d7cf24b3a2
Elias Eriksson <eliaseriksson95@gmail.com>
2023-11-22 11:46:13 +0100

    removed comment

--- Balance/Lock/index.spec.ts
+++ Balance/Lock/index.spec.ts
@@ -5,6 +5,5 @@ describe("Lock", () => {
 		const lock: weekmeter.Balance.Lock = { date: "2023-01-01" }
 		expect(weekmeter.Balance.Lock.is(lock)).toEqual(true)
 		expect(weekmeter.Balance.Lock.is((({ date, ...lock }) => lock)(lock))).toEqual(false)
-		// expect(weekmeter.Balance.Lock.is((({ user, ...lock }) => lock)(lock))).toEqual(false)
 	})
 })
