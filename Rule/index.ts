0987f86545a80f5e90c8755a07c29ebd7e912ca9
Linus Johansson <linus@issuefab.com>
2024-01-31 11:33:47 +0100

    types on expected overload fix

--- Rule/index.ts
+++ Rule/index.ts
@@ -1,5 +1,4 @@
 import { isoly } from "isoly"
-import { userwidgets } from "@userwidgets/model"
 import { isly } from "isly"
 import { Profile } from "../User/Profile"
 import { Action } from "./Action"
@@ -35,17 +34,17 @@ export namespace Rule {
 	}
 
 	export function expected(
-		user: { email: userwidgets.Email; properties?: Profile.Property[] },
+		user: Pick<Profile, "email" | "properties">,
 		dates: isoly.Date[] | isoly.DateRange,
 		rules: Base[]
 	): isoly.TimeSpan
 	export function expected(
-		user: { email: userwidgets.Email; properties?: Profile.Property[] },
+		user: Pick<Profile, "email" | "properties">,
 		dates: isoly.Date[] | isoly.DateRange,
 		rules: (Rule | string | Base)[]
 	): isoly.TimeSpan | undefined
 	export function expected(
-		user: { email: userwidgets.Email; properties?: Profile.Property[] } | Profile,
+		user: Pick<Profile, "email" | "properties">,
 		dates: isoly.Date[] | isoly.DateRange,
 		raw: (Rule | string | Base)[]
 	): isoly.TimeSpan | undefined {
