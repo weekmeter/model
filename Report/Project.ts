9e828084b7e189d7d098e3aa05674704cb6ff4d6
Linus <linus.a.johansson@protonmail.com>
2024-10-24 13:18:22 +0200

    project type complete

--- Report/Project.ts
+++ Report/Project.ts
@@ -1,14 +1,25 @@
 import { isoly } from "isoly"
-import { Time } from "../Time"
+import { isly } from "isly"
+import { Code } from "../Code"
+import { Modified } from "../Modified"
 import { Base } from "./Base"
 import { Project as ProjectCreatable } from "./Creatable/Project"
-import { Times } from "./Times"
+import { Type } from "./Type"
 
-export type Project = { [type in Time.Type]: Times<Time & { type: type }> } & {
+export type Project = {
 	total: isoly.TimeSpan
 	dates: isoly.DateRange
-} & Base
+} & Base &
+	Omit<Project.Creatable, "date">
 
 export namespace Project {
 	export import Creatable = ProjectCreatable
+	export const type = isly.object<Project>({
+		total: isly.fromIs("isoly.TimeSpan", isoly.TimeSpan.is),
+		dates: isly.fromIs("isoly.DateRange", isoly.DateRange.is),
+		modified: Modified.type,
+		type: Type.Project.type,
+		client: Code.type,
+		adjustment: isly.fromIs("Isoly.TimeSpan", isoly.TimeSpan.is).optional(),
+	})
 }
