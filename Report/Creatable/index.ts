3f06801a0edf70053c4358d26938172f3c605c44
Linus Johansson <linus@issuefab.com>
2024-10-28 13:51:25 +0100

    report project type

--- Report/Creatable/index.ts
+++ Report/Creatable/index.ts
@@ -1,9 +1,10 @@
+import { Project as CreatableProject } from "./Project"
 import { Salary as CreatableSalary } from "./Salary"
 
-export type Creatable = Creatable.Salary
+export type Creatable = Creatable.Project | Creatable.Salary
 export namespace Creatable {
-	export type Salary = CreatableSalary
-	export const Salary = CreatableSalary
+	export import Project = CreatableProject
+	export import Salary = CreatableSalary
 	export const type = CreatableSalary.type
 	export const is = type.is
 	export const flaw = type.flaw
---
+++ Report/Project.ts
@@ -0,0 +1,18 @@
+import { isly } from "isly"
+import { Time } from "../Time"
+import { Base } from "./Base"
+import { Project as Creatable } from "./Creatable/Project"
+
+export type Project = {
+	time: Time.Work[]
+} & Base &
+	Creatable
+export namespace Project {
+	export const type = isly.intersection<Project, Omit<Project, keyof Base | keyof Creatable>, Base, Creatable>(
+		isly.object<Omit<Project, keyof Base | keyof Creatable>>({ time: isly.array(Time.Work.type) }),
+		Base.type,
+		Creatable.type
+	)
+	export const is = type.is
+	export const flaw = type.flaw
+}
