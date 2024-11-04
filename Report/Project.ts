b9133ce1069b036306317cb6a9d6c3454d0c8328
Elias Eriksson <eliaseriksson95@gmail.com>
2024-11-04 10:12:31 +0100

    added export

--- Report/Project.ts
+++ Report/Project.ts
@@ -1,13 +1,14 @@
 import { isly } from "isly"
 import { Time } from "../Time"
 import { Base } from "./Base"
-import { Project as Creatable } from "./Creatable/Project"
+import { Project as ProjectCreatable } from "./Creatable/Project"
 
 export type Project = {
 	time: Time.Work[]
 } & Base &
-	Creatable
+	Project.Creatable
 export namespace Project {
+	export import Creatable = ProjectCreatable
 	export const type = isly.intersection<Project, Omit<Project, keyof Base | keyof Creatable>, Base, Creatable>(
 		isly.object<Omit<Project, keyof Base | keyof Creatable>>({ time: isly.array(Time.Work.type) }),
 		Base.type,
