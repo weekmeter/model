75924f36bb862b33325e2ae6ff9f3c59bd7c0526
Linus <linus.a.johansson@protonmail.com>
2024-11-07 15:28:18 +0100

    const type for report/creatable

--- Report/Creatable/index.ts
+++ Report/Creatable/index.ts
@@ -1,3 +1,4 @@
+import { isly } from "isly"
 import { Project as CreatableProject } from "./Project"
 import { Salary as CreatableSalary } from "./Salary"
 
@@ -5,7 +6,7 @@ export type Creatable = Creatable.Project | Creatable.Salary
 export namespace Creatable {
 	export import Project = CreatableProject
 	export import Salary = CreatableSalary
-	export const type = CreatableSalary.type
+	export const type = isly.union<Creatable>(CreatableSalary.type, CreatableProject.type)
 	export const is = type.is
 	export const flaw = type.flaw
 }
