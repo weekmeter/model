4cda45d67e8b9a16febcd74edb5075e97d4c068b
Elias Eriksson <eliaseriksson95@gmail.com>
2024-11-19 13:40:58 +0100

    current lock -> locks and brought back old lock

--- Balance/index.spec.ts
+++ Balance/index.spec.ts
@@ -4,10 +4,12 @@ describe("Balance", () => {
 	it("is", () => {
 		const balance: weekmeter.Balance = {
 			value: { hours: 1, minutes: 30 },
-			lock: { salary: "2023-01-01", projects: { "client/project": "2024-01-01" } },
+			lock: "2023-01-01",
+			locks: { salary: "2023-01-01", projects: { "client/project": "2024-01-01" } },
 		}
 		expect(weekmeter.Balance.is(balance)).toEqual(true)
 		expect(weekmeter.Balance.is((({ lock, ...balance }) => balance)(balance))).toEqual(true)
+		expect(weekmeter.Balance.is((({ locks, ...balance }) => balance)(balance))).toEqual(true)
 		expect(weekmeter.Balance.is((({ value, ...balance }) => balance)(balance))).toEqual(false)
 	})
 })
--- Balance/index.ts
+++ Balance/index.ts
@@ -4,22 +4,23 @@ import { Day } from "../Day"
 
 export interface Balance {
 	value: isoly.TimeSpan
-	lock?: {
+	// deprecated property for backwards compatibility with the old old app
+	lock?: isoly.Date
+	locks?: {
 		salary?: isoly.Date
 		projects?: Partial<Record<Day.Project.Work, isoly.Date>>
 	}
 }
 export namespace Balance {
+	const date = isly.fromIs<isoly.Date>("isoly.Date", isoly.Date.is)
 	export const type = isly.object<Balance>({
 		value: isly.fromIs<isoly.TimeSpan>("isoly.TimeSpan", isoly.TimeSpan.is),
-		lock: isly
-			.object<Required<Balance>["lock"]>({
-				salary: isly.fromIs<isoly.Date>("isoly.Date", isoly.Date.is).optional(),
+		lock: date.optional(),
+		locks: isly
+			.object<Required<Balance>["locks"]>({
+				salary: date.optional(),
 				projects: isly
-					.record<Required<Required<Balance>["lock"]>["projects"]>(
-						Day.Project.Work.type,
-						isly.fromIs<isoly.Date>("isoly.Date", isoly.Date.is).optional()
-					)
+					.record<Required<Required<Balance>["locks"]>["projects"]>(Day.Project.Work.type, date.optional())
 					.optional(),
 			})
 			.optional(),
